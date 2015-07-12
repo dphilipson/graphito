9feedf13786e6e888b4602166ed6302b12822249
dphilipson <dphilipson@palantir.com>
2015-07-11 21:24:42 -0700

    Some setup
    * Added libraries jQuery and Raphael
    * Add a container element which fills the screen
    * Add a Raphael paper element

--- src/graphito/core.cljs
+++ src/graphito/core.cljs
@@ -6,4 +6,27 @@
 
 (enable-console-print!)
 
-(println "Hello world!")
+(def $ js/$)
+(def Raphael js/Raphael)
+
+(defn disable-touchmove [$element]
+  (.on $element "touchmove" #(.preventDefault %)))
+
+(defn add-background [paper width height]
+  (let [rect (.rect paper 0 0 width height)]
+    (.attr rect (js-obj "fill" "270-#ccc-#888"
+                        "stroke" 0))))
+
+(defn setup-paper [$container]
+  (disable-touchmove $container)
+  (let [width (.width $container)
+        height (.height $container)
+        containerElem (.get $container 0)
+        paper (Raphael containerElem width height)]
+    (add-background paper width height)
+    paper))
+
+(defn ^:export inhabit [selector]
+  (let [$container ($ selector)
+        paper (setup-paper $container)]
+    nil))
