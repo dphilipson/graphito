571b16a4acc4765cef3b9407a72cc6f2414c57ea
dphilipson <dphilipson@palantir.com>
2015-07-11 22:17:52 -0700

    Some code structure for representing state

--- src/graphito/core.cljs
+++ src/graphito/core.cljs
@@ -9,24 +9,59 @@
 (def $ js/$)
 (def Raphael js/Raphael)
 
-(defn disable-touchmove [$element]
+;; Paper setup
+
+(defn disable-touchmove! [$element]
   (.on $element "touchmove" #(.preventDefault %)))
 
-(defn add-background [paper width height]
+(defn add-background! [paper width height]
   (let [rect (.rect paper 0 0 width height)]
     (.attr rect (js-obj "fill" "270-#ccc-#888"
                         "stroke" 0))))
 
-(defn setup-paper [$container]
-  (disable-touchmove $container)
-  (let [width (.width $container)
-        height (.height $container)
-        containerElem (.get $container 0)
+(defn setup-paper! [$container width height]
+  (disable-touchmove! $container)
+  (let [containerElem (.get $container 0)
         paper (Raphael containerElem width height)]
-    (add-background paper width height)
+    (add-background! paper width height)
     paper))
+
+;; State management
+
+(defn initial-state [paper width height]
+  {:paper paper
+   :view-width width
+   :view-height height
+   :camera-x 0
+   :camera-y 0
+   :nodes []})
+
+(defn create-node-element!
+  "Adds a new node element to the paper and returns it."
+  [paper]
+  (let [elem (.circle paper 20 20 10)]
+    (.attr elem (js-obj "fill" "blue"
+                        "stroke" "gray"
+                        "stroke-width" 2))
+    elem))
+
+(defn add-node!
+  "Adds a new node element to the paper and returns an updated state which
+  references this element." 
+  [state title x y]
+  (let [elem (create-node-element! (:paper state))
+        new-node {:title title
+                  :x x
+                  :y y
+                  :element elem}]
+    (update state :nodes conj new-node)))
+
+;; Exported function to do magic
 
 (defn ^:export inhabit [selector]
   (let [$container ($ selector)
-        paper (setup-paper $container)]
-    nil))
+        width (.width $container)
+        height (.height $container)
+        paper (setup-paper! $container width height)
+        state (initial-state paper width height)]
+    (add-node! state "Hello Node" 0 0)))
